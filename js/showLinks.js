59c2277fdfc6b4fd6f58861861912d42cf870093
m0therH@cker <94325189+radiocosmopolit@users.noreply.github.com>
2023-02-05 05:00:54 +0300

    1234

--- js/showLinks.js
+++ js/showLinks.js
@@ -49,8 +49,7 @@ var showLinks = ( function(){
 
         pan.onclick = 'window.open('+data[i].link+");";
         pan.classList.add("showlinks")
-        pan.innerHTML = '<script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-post="'+
-        data[i].username+'/'+data[i].message_id+'" data-width="100%" data-userpic="false" data-dark="1"></script>'
+        pan.innerHTML = '<script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-post="telegram/83" data-width="100%"></script>'
         /*
         //pan.href = data[i].link;
         name = document.createElement('a')
