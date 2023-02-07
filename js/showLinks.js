e88021a5184db6701f69b121db029fd5add4b6ed
m0therH@cker <94325189+radiocosmopolit@users.noreply.github.com>
2023-02-07 05:07:09 +0300

    123

--- js/showLinks.js
+++ js/showLinks.js
@@ -45,7 +45,8 @@ var showLinks = ( function(){
         pan.setAttribute("data-newsid", data[i].username+"/"+data[i].message_id)
         g = document.createElement('script')
         g.async = true
-        g.setAttribute('src', "https://telegram.org/js/telegram-widget.js?21")
+        //g.setAttribute('src', "https://telegram.org/js/telegram-widget.js?21")
+        g.setAttribute('src', "telegramWidget.js")
         g.setAttribute('data-telegram-post', data[i].username+"/"+data[i].message_id)
         g.setAttribute('data-width', "100%")
         g.setAttribute('data-dark', "1")
