9ceb711d62b215f66302d9cd46a6447c5159d417
m0therH@cker <94325189+radiocosmopolit@users.noreply.github.com>
2023-02-17 15:27:52 +0300

    wer

--- js/pinned.js
+++ js/pinned.js
@@ -34,9 +34,9 @@
         //a.style.color = "white"
         a.innerHTML = key
         a.style.color = "transparent";    
-        a.style.webkitBackgroundClip: text;
-        a.style.backgroundClip: text;
-        a.style.backgroundImage: linear-gradient(90deg, red, orange, fuchsia);
+        a.style.webkitBackgroundClip = "text";
+        a.style.backgroundClip="text";
+        a.style.backgroundImage="linear-gradient(90deg, red, orange, fuchsia)";
         a.onclick = function(){
           window.open(pinned.listNewbi[key], '_blank').focus()
         }
