a2a815136db622e76f0dd2290da118a09ab39815
m0therH@cker <94325189+radiocosmopolit@users.noreply.github.com>
2023-03-08 19:17:28 +0300

    12

--- js/linkCatcher.js
+++ js/linkCatcher.js
@@ -3,6 +3,8 @@
 // we need catch something hrefs, and exec it
 window.addEventListener("load", function(){linkCatcher()});
 
+let linker = ["8march"]
+
 function linkCatcher(){
 
     console.log(document.referrer)
