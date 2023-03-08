909916acfb4e0178dc7589982b4a4bfea03ffd29
m0therH@cker <94325189+radiocosmopolit@users.noreply.github.com>
2023-03-08 04:47:42 +0300

    create linkcatcher !evol!

---
+++ js/linkCatcher.js
@@ -0,0 +1,9 @@
+// thats is alpha version/ creater !EVOL!
+
+// we need catch something hrefs, and exec it
+window.addEventListener("load", function(){linkcatcher()});
+
+function linkcatcher(){
+
+    console.log(window.location.hash,window.location.search)
+}
